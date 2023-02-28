821a85971d0b2ebf3292d3ae16e972070e73de78
yzp0n <yzp0n@coe.ad.jp>
2023-02-28 23:05:26 +0900

    code compiles and runs

--- generate.go
+++ generate.go
@@ -1,4 +1,5 @@
 package main
 
-//go:generate controller-gen crd:trivialVersions=true rbac:roleName=manager-role paths="./..." output:crd:artifacts:config=config/crd
+//go:generate controller-gen crd paths="./..." output:crd:artifacts:config=charts/kmgm-issuer/controller-gen/crd
+//go:generate sh -c "controller-gen rbac:roleName=manager-role paths=\"./...\" output:stdout > charts/kmgm-issuer/controller-gen/role.yaml"
 //go:generate controller-gen object:headerFile="hack/boilerplate.go.txt" paths="./..."
