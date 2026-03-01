39c6359ea2d21f7d1956efbd64e5a3901c120009
yzp0n <yzp0n@coe.ad.jp>
2026-03-01 13:51:14 +0000

    Ensure script is sourced by checking shell context

--- setup_testenv.sh
+++ setup_testenv.sh
@@ -4,8 +4,18 @@
 # Usage:
 #   source setup_testenv.sh
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  echo "This script must be sourced, not executed: source ${BASH_SOURCE[0]}"
+is_sourced=false
+
+if [[ -n "${ZSH_VERSION-}" ]]; then
+  case "${ZSH_EVAL_CONTEXT-}" in
+    *:file) is_sourced=true ;;
+  esac
+elif [[ -n "${BASH_VERSION-}" ]]; then
+  [[ "${BASH_SOURCE[0]-}" != "${0}" ]] && is_sourced=true
+fi
+
+if [[ "${is_sourced}" != "true" ]]; then
+  echo "This script must be sourced, not executed"
   exit 2
 fi
 
